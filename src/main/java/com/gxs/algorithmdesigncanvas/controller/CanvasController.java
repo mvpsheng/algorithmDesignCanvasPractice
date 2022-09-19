c0b090f5ce24e503037e64b80369a960608f1e7d
mvpsheng <2711136701@qq.com>
2022-09-19 18:01:51 +0800

    '添加跨域请求'

--- src/main/java/com/gxs/algorithmdesigncanvas/controller/CanvasController.java
+++ src/main/java/com/gxs/algorithmdesigncanvas/controller/CanvasController.java
@@ -3,29 +3,31 @@ package com.gxs.algorithmdesigncanvas.controller;
 import com.gxs.algorithmdesigncanvas.entity.Canvas;
 import com.gxs.algorithmdesigncanvas.mapper.CanvasMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * FileName: CanvasController
  * author: gxs
  * Date: 2022/8/23  11:19
  */
-@Controller
+@RestController
 @RequestMapping("/canvas")
 public class CanvasController {
 
     @Autowired
     CanvasMapper canvasMapper;
+    static int counter = 0;
 
-//    @GetMapping(value = "/canvasList")
-    @RequestMapping("/canvasList")
-    public String getCanvasList() {
-        System.out.println("canvasList");
-        return "login";
+    @GetMapping("/canvasList")
+    @CrossOrigin(origins = "http://localhost:8080/canvas",allowCredentials = "true")
+    public List<Canvas> getCanvasList() {
+        counter++;
+        System.out.println("发生了依次canvasList请求" + counter + "次");
+
+        return canvasMapper.getAllCanvas();
     }
 
     @GetMapping(value = "/deleteCanvas")
@@ -34,22 +36,30 @@ public class CanvasController {
         canvasMapper.delete(1);
         return canvasMapper.getOne(1);
     }
-    @GetMapping(value = "/addCanvas")
-    public String addCanvasList() {
+    @PostMapping(value = "/addCanvas")
+    public String addCanvasList(HttpServletRequest request) {
+        System.out.println("又被访问到");
+        String canvasName = request.getParameter("canvasName");
+        String constraints = request.getParameter("constraints");
+        String ideas = request.getParameter("ideas");
+        String complexity = request.getParameter("complexity");
+        String password = request.getParameter("password");
+        System.out.println(canvasName + " canvasName");
+        System.out.println(ideas + " ideas");
 //        @Insert("INSERT INTO canvas(canvas_name,constraints,ideas,complexity,code,tests,finished) VALUES(#{canvasName}, #{constraints}, #{ideas}" +
 //            ", #{complexity}, #{code}, #{tests}, #{finished})")
-        System.out.println("开始插入数据。。。。。。。。。。");
-        canvasMapper.insert(Canvas.builder().canvasName("gxs1").constraints("加入约束")
-                .ideas("ideas:....").complexity("time..memory..").code("code...")
-                .tests("tests...").finished(true).build());
-        canvasMapper.insert(Canvas.builder().canvasName("gxs2").constraints("加入约束")
-                .ideas("ideas:....").complexity("time..memory..").code("code...")
-                .tests("tests...").finished(true).build());
-        canvasMapper.insert(Canvas.builder().canvasName("gxs3").constraints("加入约束")
-                .ideas("ideas:....").complexity("time..memory..").code("code...")
-                .tests("tests...").finished(false).build());
-        System.out.println("canvasList-------------");
-        System.out.println(Arrays.toString(canvasMapper.getAllCanvas().toArray()));
-        return "userMainPage.html";
+//        System.out.println("开始插入数据。。。。。。。。。。");
+//        canvasMapper.insert(Canvas.builder().canvasName("gxs1").constraints("加入约束")
+//                .ideas("ideas:....").complexity("time..memory..").code("code...")
+//                .tests("tests...").finished(true).build());
+//        canvasMapper.insert(Canvas.builder().canvasName("gxs2").constraints("加入约束")
+//                .ideas("ideas:....").complexity("time..memory..").code("code...")
+//                .tests("tests...").finished(true).build());
+//        canvasMapper.insert(Canvas.builder().canvasName("gxs3").constraints("加入约束")
+//                .ideas("ideas:....").complexity("time..memory..").code("code...")
+//                .tests("tests...").finished(false).build());
+//        System.out.println("canvasList-------------");
+//        System.out.println(Arrays.toString(canvasMapper.getAllCanvas().toArray()));
+        return "congratulations";
     }
 }
